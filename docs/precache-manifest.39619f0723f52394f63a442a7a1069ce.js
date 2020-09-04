ea66651726f1be7ec938ce88b9b9876630b59d51
carlos carvajal <carlos12150@hotmail.com>
2020-09-04 12:01:42 -0500

    docs agregado

---
+++ docs/precache-manifest.39619f0723f52394f63a442a7a1069ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931177dc0d018fd99ac838a662ed9de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7456f8d26135dba64c7",
+    "url": "/static/css/main.0e63bf58.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7456f8d26135dba64c7",
+    "url": "/static/js/main.e3dfdd43.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
